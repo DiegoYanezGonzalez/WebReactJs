b423b96f1df02c9f062c078b00b44a0564465033
diego yañez <diego.y8@gmail.com>
2021-04-02 14:32:38 -0300

    ReviewFuntionRigthBringningProductsFromCart

--- src/components/CheckoutForm/Review.js
+++ src/components/CheckoutForm/Review.js
@@ -4,26 +4,30 @@ import accounting from 'accounting';
 import {useStateValue} from "../../StateProvider";
 
 const Review = () => {
-  const [ {basket }, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <>
-      <Typography variant="h6" gutterBottom>
-        Order SUMMARY
+      <Typography variant='h6' gutterBottom>
+        Order summary
       </Typography>
-      <List>
-        {
-          basket?.map((product) => {
-            <ListItem key={product.name}>
-             <ListItemText primary={product.name}/>
-            </ListItem>
-          })
-        }
-
+      <List disablePadding>
+        {basket?.map((product) => (
+          <ListItem style={{ padding: "10px 0" }} key={product.name}>
+            <ListItemText primary={product.name} secondary={`Qty : ${1}`} />
+            <Typography variant='body2'>
+              {accounting.formatMoney(product.price, "$")}
+            </Typography>
+          </ListItem>
+        ))}
+        <ListItem style={{ padding: "10px 0" }}>
+          <ListItemText primary='Total' />
+          <Typography variant='subtitle1' style={{ fontWeight: 700 }}>
+            {accounting.formatMoney(getBasketTotal(basket), "$")}
+          </Typography>
+        </ListItem>
       </List>
-     
-      
     </>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
